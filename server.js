1116fc2e838fd190b5a452a28a416b0c8aa12081
Gaurav Avula <avulagaurav@gmail.com>
2023-10-13 11:59:42 -0400

    added new addBudget page for easy insertion,check similar colors,checks same names

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 app.use(bodyParser.json());
+const ColorDifference = require('color-difference');
+
 
 
 const url = process.env.MONGODB_URL
@@ -53,14 +55,44 @@ app.get('/budget', async (req, res) => {
   
   app.post('/budget', async (req, res) => {
     try {
-      const { title, budget, color } = req.body;
-      const newEntry = new Budget({ title, budget, color });
-      await newEntry.save();
-      res.status(201).json(newEntry);
+        const { title, budget, color } = req.body;
+
+        // Check if a budget entry with the same title exists
+        const existingTitleEntry = await Budget.findOne({ title });
+
+        // Check if a budget entry with the same color exists
+        const existingColorEntry = await Budget.findOne({ color });
+
+        // Check if a similar color exists
+        const colors = await Budget.find({}, 'color');
+        const similarColorEntry = colors.find((entry) => {
+            const similarity = ColorDifference.compare(entry.color, color);
+            // Adjust the threshold as needed; lower values make colors more similar
+            const similarityThreshold = 10;
+            return similarity <= similarityThreshold;
+        });
+
+        if (existingTitleEntry) {
+            return res.status(400).json({ error: 'Title already exists' });
+        }
+
+        if (existingColorEntry) {
+            return res.status(400).json({ error: 'Color already exists' });
+        }
+
+        if (similarColorEntry) {
+            return res.status(400).json({ error: 'Similar color already exists' });
+        }
+
+        const newEntry = new Budget({ title, budget, color });
+        await newEntry.save();
+        res.status(201).json(newEntry);
     } catch (err) {
-      console.error('Error adding data:', err);
-      res.status(500).json({ error: 'Internal Server Error' });
+        console.error('Error adding data:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-  });
+});
 
  app.use('/',express.static('public'));
+
+ app.use('/addBudget',express.static('public/addBudget.html'));
